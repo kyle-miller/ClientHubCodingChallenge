e733f3beaeda0792b93bc7f68e8b68629476cb92
Kyle Miller <frgttnsin@gmail.com>
2018-06-07 20:56:19 -0500

    Updates based on requirements
    https://github.com/pointsixsolutions/fizzbuzzchallenge

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -5,18 +5,34 @@
  * @param {!Object} res Cloud Function response context.
  */
  
-numberRePattern = /^\d+$/;
+numberRePattern = /^[-]?\d+$/;
  
 exports.fizzBuzz = (req, res) => {
 	// Example input: 100
 	try {
-		if (req.body === undefined || req.body.number === undefined || !numberRePattern.test(req.body.number.toString())) {
-			res.status(400).send('Post a number greater than 1');
+		var input;
+
+		if (req.body === undefined || req.body.max_range === undefined) {
+			input = Number(100);
+		} else if (!numberRePattern.test(req.body.max_range.toString())) {
+			res.status(400).send("Post a max_range greater that's a number");
+			return;
 		} else {
-			var input = Number(req.body.number);
-			var fizzBuzzStr = ((input % 3 ? '' : 'Fizz') + (input % 5 ? '' : 'Buzz')) || input.toString();
-			res.status(200).send(fizzBuzzStr);
+			input = Number(req.body.max_range);
 		}
+
+		var fizzBuzzStr = '';
+		if (input >= 0) {
+			for (var i = 0; i <= input; i++) {
+				fizzBuzzStr += (((i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz')) || i.toString()) + "\n";
+			}
+		} else {
+			for (var i = 0; i >= input; i--) {
+				fizzBuzzStr += (((i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz')) || i.toString()) + "\n";
+			}
+		}
+
+		res.status(200).send(fizzBuzzStr);
 	} catch (e) {
 		console.log(e.stack);
 		res.status(500).send('Something went wrong: ' + e.message);
